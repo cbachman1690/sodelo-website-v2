8f71d1649627dccd24961d2ac70b8a66cbd929f8
CA Bachman <69227823+cbachman1690@users.noreply.github.com>
2026-04-26 19:24:35 -0400

    Add GitHub Pages base path

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isGithubPages = process.env.GITHUB_ACTIONS === 'true'
+
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
+  basePath: isGithubPages ? '/sodelo-website-v2' : '',
+  assetPrefix: isGithubPages ? '/sodelo-website-v2/' : '',
 }
 
 module.exports = nextConfig
